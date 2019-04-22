fbdbc652865a06be0639bfb0984810dbf152b2ef
Cody <cody.tracy92@gmail.com>
2019-04-22 11:54:15 -0500

    added in some comments for readability and the handlebars basic main

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+//Requiring express
 var express = require("express");
 
+//Defines the port to be used
 var PORT = process.env.PORT || 3000;
 
+//Defines the app using express
 var app = express();
 
 //Allows for static content for the app from the 'public' directory.
@@ -18,10 +21,13 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars",exphbs({defaultLayout: "main"}));
 app.set("view engine","handlebars");
 
+//Imports the routes from the controller
 var routes = require("./controllers/burgers_controller.js");
 
+//Use said routes
 app.use(routes);
 
+//Starts the server
 app.listen(PORT, function(){
     console.log("Server is listening on http://localhost:" + PORT);
 });
