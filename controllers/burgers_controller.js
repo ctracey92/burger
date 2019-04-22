739aabfc10ae39564a4499c8d224f0fe91dbe6df
Cody <cody.tracy92@gmail.com>
2019-04-22 12:20:46 -0500

    finished the routing for now

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,7 @@ var express = require("express");
 
 var router = express.Router();
 
+//Get all the burgers
 router.get("/",function(req,res){
     burger.all(function(data){
         var burgerObject = {
@@ -12,5 +13,38 @@ router.get("/",function(req,res){
     });
     res.render('index',burgerObject);
 });
+
+//Add a new burger
+router.post("/api/burgers",function(req,res){
+    burger.insertOne([
+        "name"
+    ],
+    [
+        req.body.name
+    ],
+    function(result){
+        res.json({id: result.insertId});
+    });
+});
+
+//Change the devoured status
+router.put("/api/burgers/:id",function(req,res){
+    var burgerId = req.params.id;
+
+    burger.updateOne(
+        burgerId,function(result){
+            if (result.changedRows == 0){
+                //If no changes were made 404
+                return res.status(404).end();
+            }
+            else {
+                res.status(200).end();
+            }
+        }
+    )
+})
+
+
+
 
 module.exports = router;
