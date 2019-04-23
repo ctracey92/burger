246a1714117437881b8829cbac35bc9667eea8d4
Cody <cody.tracy92@gmail.com>
2019-04-23 17:25:52 -0500

    added in ther heroku app code

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 //Require Mysql
 var mysql = require("mysql");
-
+var con;
+if (process.env.JAWSDB_URL){
+  con = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
 //Define the connection variable
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "h0usTon1",
     database: "burgers_db"
-})
+});  
+};
+
+
 
 //Create the connection
 con.connect(function(err){
